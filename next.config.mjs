0afb451add694d284c36e4a2944dad0ea3d9d90d
sacazidal <tw1nkle001@yandex.ru>
2025-01-17 02:38:37 +0700

    подключил бд

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*", // Все запросы, начинающиеся с /api
+        destination:
+          "https://pnbnwjpzuflgednrcyfl.supabase.co", // Перенаправление на Supabase
+      },
+    ];
+  },
+};
 
 export default nextConfig;
